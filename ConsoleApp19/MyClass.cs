686add0eff284a07079e60a61ec9dd1517abaf5e
aiymgulasylbekova <124632132+aiymgulasylbekova@users.noreply.github.com>
2023-12-10 01:17:19 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp19/MyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class MyClass
+    {
+        // Публичное свойство
+        public int PublicProperty { get; set; }
+
+        // Приватное свойство
+        private string PrivateProperty { get; set; }
+
+        // Конструктор без параметров
+        public MyClass()
+        {
+        }
+
+        // Конструктор с параметрами
+        private MyClass(int value)
+        {
+            PublicProperty = value;
+        }
+
+        // Публичный метод
+        public void PublicMethod()
+        {
+            Console.WriteLine("PublicMethod");
+        }
+
+        // Приватный метод
+        private void PrivateMethod()
+        {
+            Console.WriteLine("PrivateMethod");
+        }
+    }
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myClassType = typeof(MyClass);
+
+            // Имя класса
+            Console.WriteLine($"Имя класса: {myClassType.Name}");
+
+            // Конструкторы
+            ConstructorInfo[] constructors = myClassType.GetConstructors();
+            Console.WriteLine("\nКонструкторы:");
+            foreach (var constructor in constructors)
+            {
+                Console.WriteLine($"Модификатор доступа: {constructor.Attributes}");
+                Console.WriteLine($"Параметры: {string.Join(", ", constructor.GetParameters().Select(p => $"{p.ParameterType} {p.Name}"))}");
+            }
+
+            // Поля и свойства
+            Console.WriteLine("\nПоля и свойства:");
+            foreach (var property in myClassType.GetProperties())
+            {
+                Console.WriteLine($"Модификатор доступа: {property.GetAccessors().First().Attributes}");
+                Console.WriteLine($"Тип: {property.PropertyType.Name}");
+                Console.WriteLine($"Имя: {property.Name}");
+            }
+
+            // Методы
+            Console.WriteLine("\nМетоды:");
+            foreach (var method in myClassType.GetMethods())
+            {
+                Console.WriteLine($"Модификатор доступа: {method.Attributes}");
+                Console.WriteLine($"Тип возвращаемого значения: {method.ReturnType.Name}");
+                Console.WriteLine($"Имя: {method.Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
